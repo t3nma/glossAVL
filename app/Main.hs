5483f3a6a1a61576587e48cca40e84bd08f511a1
t3nma <brvnocasteleiro@gmail.com>
2021-03-08 04:53:44 +0000

    Simple text-rendering demo

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,31 @@ module Main where
 
 -- import Lib
 import Graphics.Gloss
+import Graphics.Gloss.Data.ViewPort
+import Graphics.Gloss.Interface.Environment
+
+fps :: Int
+fps = 60
+
+type ScreenSize = (Int, Int)
+type Text = (ScreenSize, Int)
+
+initNum :: IO Text
+initNum = do
+  size <- getScreenSize -- didn't really use it but now I know how to do it
+  return (size, 0)
 
 main :: IO ()
-main = display window white teste
+main = do
+  num <- initNum
+  simulate FullScreen white fps num drawNum updateNum
 
-window = InWindow "GlossTeste" (800,600) (0,0)
-teste = circleSolid 100
+drawNum :: Text -> Picture
+drawNum (_,n) = pictures [circleSolid 200,
+                          translate (-50.0) (-25.0) $ scale 0.5 0.5 $ color white (text $ show n)]
+
+updateNum :: ViewPort -> Float -> Text -> Text
+updateNum _ _ (size,n) = (size,n+1)
+
+-- window = InWindow "GlossTeste" (800,600) (0,0)
+-- teste = circleSolid 100
