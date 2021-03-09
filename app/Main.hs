9661c2fb01ac832eb392b90f75b02bff3928fb3f
t3nma <brvnocasteleiro@gmail.com>
2021-03-09 06:25:07 +0000

    - I/O demo is now working. - Using the strict package for forcing stric getContents from stdin

--- app/Main.hs
+++ app/Main.hs
@@ -4,37 +4,25 @@ import Util
 import AVLTree
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
--- import Graphics.Gloss.Interface.IO.Simulate
+import qualified System.IO.Strict as SIO (getContents)
 
 type Model a = (Tree a, [Command])
 
 main :: IO ()
 main = do
   model <- initModel
-  -- simulateIO FullScreen white 60 model drawModel updateModel
-  simulate FullScreen white 60 model drawModel updateModel
+  simulate FullScreen white 1 model drawModel updateModel
 
 initModel :: IO (Model a)
 initModel = do
-  commands <- fmap (toCmdList . lines) getContents
+  commands <- fmap (toCmdList . lines) SIO.getContents
   return (empty,commands)
 
-{-
-drawModel :: Model a -> IO Picture
-drawModel (_,[])         = do return $ text "END."
-drawModel (_,(Add a:xs)) = do return $ text ("+" ++ show a)
-drawModel (_,(Rem a:xs)) = do return $ text ("-" ++ show a)
-
-updateModel :: ViewPort -> Float -> Model a -> IO (Model a)
-updateModel _ _ (tree,[])     = do return (tree,[])
-updateModel _ _ (tree,(x:xs)) = do return (tree,xs)
--}
-
 drawModel :: Model a -> Picture
-drawModel (_,[])         = text "END."
+drawModel (_,[])         = text "END"
 drawModel (_,(Add a:xs)) = text ("+" ++ show a)
 drawModel (_,(Rem a:xs)) = text ("-" ++ show a)
 
 updateModel :: ViewPort -> Float -> Model a -> Model a
-updateModel _ _ (tree,[])     = (tree,[])
-updateModel _ _ (tree,(x:xs)) = (tree,xs)
+updateModel _ _ (tree,[])  = (tree,[])
+updateModel _ _ (tree,xs) = (tree,tail xs)
