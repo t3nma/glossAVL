f32dcc6fc7f7989f9bcfe9a3b7ce85ea3670965b
t3nma <brvnocasteleiro@gmail.com>
2021-03-17 04:48:23 +0000

    Update GlossAVL, add comentary

--- src/GlossAVL.hs
+++ src/GlossAVL.hs
@@ -78,6 +78,7 @@ rootOrig = (0,800/4)
 --- Picture utils
 ---
 
+-- Retrieve picture of node at given point.
 treeNodeAt :: Show a => a -> Point -> Bool-> Picture
 treeNodeAt k (x,y) isTarget = translate x y $ pictures node
   where
@@ -86,12 +87,15 @@ treeNodeAt k (x,y) isTarget = translate x y $ pictures node
             color c $ circleSolid nodeRadius,
             translate (-12) (-7) $ scale 0.1 0.1 (text $ show k)]
 
+-- Retrieve picture of leaf node at given point.
 leafAt :: Point -> Picture
 leafAt (x,y) = translate x y $ color leafColor $ rectangleSolid leafSize leafSize
 
+-- Retrieve picture of a line between two node points.
 treeNodeLink :: Point -> Point -> Picture
 treeNodeLink p1 p2 = line [p1,p2]
 
+-- Retrieve picture of last command applied.
 cmdLegend :: Show a => Maybe (Command a) -> Picture
 cmdLegend cmd = translate (-35) y' $ scale 0.2 0.2 $ text (cmdText cmd)
   where
@@ -102,14 +106,17 @@ cmdLegend cmd = translate (-35) y' $ scale 0.2 0.2 $ text (cmdText cmd)
 --- Auxiliary
 ---
 
+-- Retrieve size for some given tree node.
 treeSize :: Tree a -> Int
 treeSize (Node _ _ l r) = 1 + treeSize l + treeSize r
 treeSize _              = 0
 
+-- Retrieve textual representation for a Maybe (Command).
 cmdText :: Show a => Maybe (Command a) -> String
 cmdText Nothing  = " "
 cmdText (Just a) = show a
 
+-- Retrieve some node's child points in the space.
 childOrig :: Tree a -> Point -> (Point,Point)
 childOrig node (x,y) = (ptLeft,ptRight)
   where
@@ -117,21 +124,9 @@ childOrig node (x,y) = (ptLeft,ptRight)
     ptLeft  = (x-width*widthFactor/2,y-levelSpacing)
     ptRight = (x+width*widthFactor/2,y-levelSpacing)
 
----
---- Model functions
----
-
-initModel :: (Read a, Show a, Ord a) => IO (Model a)
-initModel = do
-  commands <- fmap (catMaybes . toCmdList) readLoop
-  return (empty,commands,Nothing)
-
-drawModel :: (Ord a, Show a) => Model a -> Picture
-drawModel (tree,_,prev) = pictures $ cmdPic:treePic
-  where
-    cmdPic  = cmdLegend prev
-    treePic = drawTree rootOrig tree prev
-
+-- Retrieve [Picture] representation of a given tree state,
+-- starting at the given origin point and considering the
+-- given last applied command.
 drawTree :: (Ord a, Show a) => Point -> Tree a -> Maybe (Command a) -> [Picture]
 drawTree _ Empty _      = []
 drawTree orig tree prev = worker orig tree prev []
@@ -145,6 +140,24 @@ drawTree orig tree prev = worker orig tree prev []
         curNode          = treeNodeAt k pt (k == cmdKey cmd)
         pics'            = curNode:worker ptRight right prev pics
 
+---
+--- Model functions
+---
+
+-- Read input commands and retrieve model zero.
+initModel :: (Read a, Show a, Ord a) => IO (Model a)
+initModel = do
+  commands <- fmap (catMaybes . toCmdList) readLoop
+  return (empty,commands,Nothing)
+
+-- Function that draws the model in each iteration.
+drawModel :: (Ord a, Show a) => Model a -> Picture
+drawModel (tree,_,prev) = pictures $ cmdPic:treePic
+  where
+    cmdPic  = cmdLegend prev
+    treePic = drawTree rootOrig tree prev
+
+-- Function that updates the model in each iteration.
 updateModel :: Ord a => ViewPort -> Float -> Model a -> Model a
 updateModel _ _ (tree,[],prev)  = (tree,[],prev)
 updateModel _ _ (tree,(x:xs),_) = (doCmd x tree,xs,Just x)
